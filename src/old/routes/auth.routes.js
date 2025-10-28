fcb2e1a16a8a496c07b2bccfba67e41fd6c6a48e
setiawansandi <81544477+setiawansandi@users.noreply.github.com>
2025-10-29 01:24:24 +0800

    chore: remove old folder from repo

--- src/old/routes/auth.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const ctrl = require('../controllers/auth.controller');
-
-router.post('/register', ctrl.register);
-router.post('/login', ctrl.login);
-router.post('/forgot', ctrl.forgot);
-router.post('/reset', ctrl.reset);
-
-module.exports = router;
--- src/old/routes/users.routes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const { requireAuth } = require('../middlewares/auth.middleware');
-const ctrl = require('../controllers/users.controller');
-
-router.get('/me', requireAuth, ctrl.me);
-router.patch('/me', requireAuth, ctrl.updateMe);
-
-module.exports = router;
